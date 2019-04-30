3d55243ec2825f7d5c07d52f9b62f11ed4585939
skmta <a07090072@gmail.com>
2019-04-30 17:50:55 +0900

    push test

--- app/src/main/java/com/example/myapplication/ExampleFragment.java
+++ app/src/main/java/com/example/myapplication/ExampleFragment.java
@@ -1,4 +1,36 @@
 package com.example.myapplication;
 
-public class ExampleFragment {
+import android.os.Bundle;
+import android.support.annotation.ColorRes;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.LinearLayout;
+
+
+public class ExampleFragment extends Fragment{
+    private final static String BACKGROUND_COLOR = "background_color";
+
+    public static ExampleFragment newInstance(@ColorRes int IdRes) {
+        ExampleFragment frag = new ExampleFragment();
+        Bundle b = new Bundle();
+        b.putInt(BACKGROUND_COLOR, IdRes);
+        frag.setArguments(b);
+        return frag;
+    }
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_main, null);
+        LinearLayout linearLayout = (LinearLayout) view.findViewById(R.id.fragment_main_linearlayout);
+        linearLayout.setBackgroundResource(getArguments().getInt(BACKGROUND_COLOR));
+
+        return view;
+    }
 }
--- app/src/main/java/com/example/myapplication/ExampleFragmentPagerAdapter.java
+++ app/src/main/java/com/example/myapplication/ExampleFragmentPagerAdapter.java
@@ -1,4 +1,35 @@
 package com.example.myapplication;
 
-public class ExampleFragmentPagerAdapter {
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+
+
+public class ExampleFragmentPagerAdapter extends FragmentPagerAdapter{
+    public ExampleFragmentPagerAdapter(FragmentManager fm) {
+        super(fm);
+    }
+
+    @Override
+    public Fragment getItem(int position) {
+        switch (position) {
+            case 0:
+                return ExampleFragment.newInstance(android.R.color.holo_blue_bright);
+            case 1:
+                return ExampleFragment.newInstance(android.R.color.holo_green_light);
+            case 2:
+                return ExampleFragment.newInstance(android.R.color.holo_red_dark);
+        }
+        return null;
+    }
+
+    @Override
+    public int getCount() {
+        return 3;
+    }
+
+    @Override
+    public CharSequence getPageTitle(int position) {
+        return "page" + (position + 1);
+    }
 }
